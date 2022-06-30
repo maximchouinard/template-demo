77928d6706c202c9315f860afccfe0ec7385b70d
Maxim Chouinard <maxim.chouinard@logmein.com>
2022-06-30 08:55:40 -0400

    test

--- scripts/rc_cut.sh
+++ scripts/rc_cut.sh
@@ -219,7 +219,7 @@ pull_request() {
 
   to_user=$(echo $upstream | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
   from_user=$(echo $origin | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
-  repo=$(basename `git rev-parse --show-toplevel`)
+  repo=$(basename "git rev-parse --show-toplevel")
   from_branch=$(git rev-parse --abbrev-ref HEAD)
   open "https://github.com/$to_user/$repo/pull/new/$to_user:$base_branch...$from_user:$from_branch"
 }
